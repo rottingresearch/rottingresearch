7e93ed0809d42c8f659ee8a34fc10a408bde1f7c
Faheem Khan <faheem5948@gmail.com>
2022-06-05 04:04:16 +0000

    using single quotes to be consistent throughout the codebase
    Signed-off-by: GitHub <noreply@github.com>

--- threadpool.py
+++ threadpool.py
@@ -1,14 +1,14 @@
-"""
+'''
 file: threadpool.py
 description: Inspired by http://stackoverflow.com/a/7257510
-"""
+'''
 
 from queue import Queue
 from threading import Thread
 
 
 class Worker(Thread):
-    """ Thread executing tasks from a given tasks queue """
+    ''' Thread executing tasks from a given tasks queue '''
 
     def __init__(self, tasks):
         Thread.__init__(self)
@@ -28,7 +28,7 @@ class Worker(Thread):
 
 
 class ThreadPool:
-    """ Pool of threads consuming tasks from a queue """
+    ''' Pool of threads consuming tasks from a queue '''
 
     def __init__(self, num_threads):
         self.tasks = Queue(num_threads)
@@ -36,14 +36,14 @@ class ThreadPool:
             Worker(self.tasks)
 
     def add_task(self, func, *args, **kargs):
-        """ Add a task to the queue """
+        ''' Add a task to the queue '''
         self.tasks.put((func, args, kargs))
 
     def map(self, func, args_list):
-        """ Add a list of tasks to the queue """
+        ''' Add a list of tasks to the queue '''
         for args in args_list:
             self.add_task(func, args)
 
     def wait_completion(self):
-        """ Wait for completion of all the tasks in the queue """
+        ''' Wait for completion of all the tasks in the queue '''
         self.tasks.join()
