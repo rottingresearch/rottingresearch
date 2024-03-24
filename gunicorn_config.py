da0fde3dd801e22b04c94cafc1a69cef1c4925eb
Marshal A. Miller <marshal@marshalmiller.com>
2024-03-24 19:46:27 -0400

    gunicorn config update

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,1 +1,7 @@
-workers = 10
+# gunicorn_config.py
+import multiprocessing
+
+bind = "127.0.0.1:8000"   # bind the server to localhost at port 8000
+workers = multiprocessing.cpu_count() * 2 + 1  # number of worker processes
+threads = 2  # number of threads per worker
+timeout = 120  # timeout in seconds
