01dc8a2cd34d9ba499e1673174811e1d03e46ea3
Marshal Miller <30907840+marshalmiller@users.noreply.github.com>
2024-03-24 22:26:23 -0400

    Testing Update

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,8 @@
 # gunicorn_config.py
 import multiprocessing
 
-bind = "127.0.0.1:8000"   # bind the server to localhost at port 8000
+bind = "127.0.0.1:8080"   # bind the server to localhost at port 8000
 workers = multiprocessing.cpu_count() * 2 + 1  # number of worker processes
+# workers = 1  # number of worker processes
 threads = 2  # number of threads per worker
 timeout = 120  # timeout in seconds
