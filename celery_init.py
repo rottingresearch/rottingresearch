81b1c92003036c84a3fb3a53b191d07dc0b7d3fb
Ankith Veldandi <143205588+aveldan@users.noreply.github.com>
2024-07-01 19:07:34 -0400

    Cleanup on files app.py, tasks.py and celery_init.py. Removed all refrence to Heroku

--- celery_init.py
+++ celery_init.py
@@ -11,5 +11,4 @@ def celery_init_app(app: Flask) -> Celery:
     celery_app = Celery(app.name, task_cls=FlaskTask)
     celery_app.config_from_object(app.config["CELERY"])
     celery_app.set_default()
-    app.extensions["celery"] = celery_app
     return celery_app
